5591845c7311fd813a244ed70164e0add816e562
Luis A Frigolet <lafrigolet@gmail.com>
2025-08-22 09:56:21 +0200

    Solved compilation problems

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,8 @@
-import { onCall, onRequest } from "firebase-functions/v2/https";
 import { initializeApp } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
-import {
-  listUsers,
-  addUser,
-  deleteUser,
-  setUserRole,
-  sendMagicLinkEmail,
-  magicLinkHandler
-} from "./users.js";
-import smtpConfig from "./nodemailer-conf.js";
-import nodemailer from "nodemailer";
-import { randomUUID } from "crypto";
 
 initializeApp();
 
-
-export {
-  listUsers,
-  addUser,
-  deleteUser,
-  setUserRole,
-  sendMagicLinkEmail,
-  magicLinkHandler
-};
+// Users API 
+export * from "./users.js";
 
 
