b1972808e5a8a1e0bc2a1cb5b49e3dd7b894ff3c
Luis A Frigolet <lafrigolet@gmail.com>
2025-09-07 10:53:37 +0200

    Initial working version

---
+++ src/features/navbar/Navbar.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+import Box from "@mui/material/Box";
+import { styled, alpha } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
+// Custom styled search box
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.05),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.1),
+  },
+  marginLeft: theme.spacing(2),
+  marginRight: theme.spacing(2),
+  width: "100%",
+  maxWidth: 600,
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+  },
+}));
+
+export default function Navbar() {
+  return (
+    <AppBar position="fixed" color="inherit" elevation={1}>
+      <Toolbar>
+        {/* Left Section: Logo or Menu */}
+        <IconButton edge="start" color="inherit" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ display: { xs: "none", sm: "block" }, mr: 2 }}
+        >
+          MyApp
+        </Typography>
+
+        {/* Center Section: Search */}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
+        </Search>
+
+        {/* Right Section: Profile */}
+        <Box sx={{ flexGrow: 1 }} />
+        <IconButton edge="end" color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
